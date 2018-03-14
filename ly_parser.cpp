cf507cc3ad06e30f8d9f0a4cfcda763b71fdc30b
João Vitor Araujo <joao18araujo@gmail.com>
2018-03-14 13:57:16 -0300

    Change parser to absolute pitch mode

--- ly_parser.cpp
+++ ly_parser.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
+#include <cmath>
 #include <string>
+
 
 using namespace std;
 
-
+#define BELOW_MIDDLE_C_OCTAVE 3
+#define N_SCALE 12
 
 /*
    { "c", "bis" },  // pos = 0
@@ -23,67 +26,57 @@ using namespace std;
 
 class Note{
 	public:
-		map<string, int> notes_with_accidental = {
-			{"c", 1}, {"bis", 1},
-			{"cis", 2}, {"des", 2},
-			{"d", 3},
-			{"dis", 4}, {"ees", 4},
-			{"e", 5}, {"fes", 5},
-			{"eis", 6}, {"f", 6},
-			{"fis", 7}, {"ges", 7},
-			{"g", 8},
-			{"gis", 9}, {"aes", 9},
-			{"a", 10},
-			{"ais", 11}, {"bes", 11},
-			{"b", 12}, {"ces", 12}
+		unordered_map<string, int> notes_with_accidental = {
+			{"c", 0}, {"bis", 0},
+			{"cis", 1}, {"des", 1},
+			{"d", 2},
+			{"dis", 3}, {"ees", 3},
+			{"e", 4}, {"fes", 4},
+			{"eis", 5}, {"f", 5},
+			{"fis", 6}, {"ges", 6},
+			{"g", 7},
+			{"gis", 8}, {"aes", 8},
+			{"a", 9},
+			{"ais", 10}, {"bes", 10},
+			{"b", 11}, {"ces", 11}
 		};
 
-		map<string, int> notes = {{"c", 1}, {"d", 2}, {"e", 3}, {"f", 4}, {"g", 5}, {"a", 6}, {"b", 7}};
-
+		unordered_map<string, int> notes = {{"c", 0}, {"d", 1}, {"e", 2}, {"f", 3}, {"g", 4}, {"a", 5}, {"b", 6}};
 
 		int duration;
 		string note;
+    string accidental;
 		int octave;
-		string modifier;
 		int midi_number;
 
-		Note(Note& prev, const string& m_note, const string& m_accidental, const string& m_modifier, const int& m_duration) {
-			if(not m_duration)
+		Note(Note& prev, const string& note, const string& accidental, const string& modifier, const int& duration) {
+			if(not duration)
 				this->duration = prev.duration;
+      else
+        this->duration = duration;
 
-			note = m_note;
-			octave = prev.closest_octave(note);
-			for (auto& c : m_modifier){
+			this->note = note;
+      this->accidental = accidental;
+			this->octave = BELOW_MIDDLE_C_OCTAVE;
+			for (auto& c : modifier){
 				if (c == '\'')
-					octave++;
+					this->octave++;
 				else if (c == ',')
-					octave--;
+					this->octave--;
 			}
 
-
+      this->midi_number = octave * N_SCALE + notes_with_accidental[this->full_note()];
 		}
 
-		int closest_octave(string note){
-			int this_note_number = notes[this->note];
-			int note_number = notes[note];
-			int prev_note_number = note_number - 7;
-			int next_note_number = note_number + 7;
-
-			int octave = this->octave;
-			if((7 + note_number - this_note_number)%7 >= 4){
-				if(this_note_number < note_number)
-					octave--;
-			}else if(this_note_number > note_number){}
-				
-			return octave;
-		}
+    string full_note(){
+      return this->note + this->accidental;
+    }
 };
 
 Note parse_note(Note prev, string s){
 	string accidental = "", modifier = "";
 	string note = "";
 	int duration = 0;
-
 
 	note += s[0];
 	s.erase(s.begin());
--- test-midi.ly
+++ test-midi.ly
@@ -15,30 +15,30 @@ trackAchannelA = {
 
 
   \key c \major
-    
-  \time 4/4 
-  
+
+  \time 4/4
+
 
   \key c \major
-  
-  \tempo 4 = 120 
-  
+
+  \tempo 4 = 120
+
 }
 
 trackAchannelB = \relative c {
-  g4 c, g'' c, 
+  g,4 c, g c
   | % 2
-  g'' c, g'' c, 
+  g' c' g'' c''
   | % 3
-  g c, a c, 
+  g' c' a c
   | % 4
-  g c, d 
+  g, c, d, 
 }
 
 trackA = <<
 
   \clef bass
-  
+
   \context Voice = voiceA \trackAchannelA
   \context Voice = voiceB \trackAchannelB
 >>

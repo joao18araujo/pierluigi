dcf41d0bf4c97c36e748312a2012ac90f7255419
João Vitor Araujo <joao18araujo@gmail.com>
2018-04-18 14:39:29 -0300

    Add compatibility to dotted note

--- src/note_reader.cpp
+++ src/note_reader.cpp
@@ -6,19 +6,20 @@ Note NoteReader::string_to_note(Note prev, string s){
 
   string accidental = "", modifier = "";
 	string note = "";
-	int duration = 0;
+	int duration = 0, dots = 0;
   int octave = BELOW_MIDDLE_C_OCTAVE;
 
 	note += s[0];
-  //TODO lidar com ponto de aumento
 	s.erase(s.begin());
-	for (auto& c : s){
+	for (auto & c : s){
 		if (isdigit(c)){
-			duration = 0;
+			duration *= 10;
 			duration += (c - '0');
 		} else if (isalpha(c))
 			accidental += c;
-		else
+		else if(c == '.')
+      dots++;
+    else
 			modifier += c;
 	}
 
@@ -28,9 +29,17 @@ Note NoteReader::string_to_note(Note prev, string s){
     else if (c == ',')
       octave--;
   }
-
   if(not duration)
     duration = prev.duration;
+
+  duration = (duration ? 16 / duration : duration);
+  //TODO setar valor mínimo (semicolcheia atualmente)
+
+  int increment = duration / 2;
+  while(dots--){
+    duration += increment;
+    increment /= 2;
+  }
 
   accidental = regex_replace(accidental, regex("is"), "#");
   accidental = regex_replace(accidental, regex("es"), "\u266D");
@@ -52,7 +61,7 @@ string NoteReader::note_to_string(Note note){
     while(octave_diff++)
       s += ",";
   }
-  s += to_string(note.duration);
+  s += to_string((note.duration ? 16 / note.duration : note.duration));
 
   return s;
 }
